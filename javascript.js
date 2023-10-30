746c319ce2927a2fb6c5dacdb6119eddbaa63c66
Dexter Crago <jodieanddexter@gmail.com>
2023-10-30 16:25:42 -0700

    Update calculator display when buttons are pressed

--- javascript.js
+++ javascript.js
@@ -12,3 +12,22 @@ let secondNumber;
 function operate(a, op, b) {
     return operators[op](a, b);
 }
+
+const display = document.querySelector('#display');
+const numbers = document.querySelectorAll('.number');
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        display.textContent += number.id;
+    });
+});
+const opButtons = document.querySelectorAll('.operator');
+opButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        display.textContent += ' ' + button.id + ' ';
+    });
+});
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', () => {
+    let equation = display.textContent;
+    console.log(equation);
+});
