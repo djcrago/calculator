73e3e2b7300b566fcec10a09bfe020d3fb71b8fb
Dexter Crago <jodieanddexter@gmail.com>
2023-11-06 09:44:52 -0700

    Make display box flex vertically
    The digits would start to overflow the display box before.

--- javascript.js
+++ javascript.js
@@ -11,6 +11,28 @@ let secondNumber;
 
 function operate(a, op, b) {
     return operators[op](+a, +b);
+};
+
+function equals() {
+    let equation = display.textContent;
+    let eqArray = equation.split(' ');
+    let answer;
+    console.log(eqArray);
+    console.log(eqArray.length);
+    if(eqArray.length <= 3) {
+        answer = operate(eqArray[0], eqArray[1], eqArray[2]);
+    } else {
+        answer = eqArray.reduce((sum, current) => {
+            current = eqArray[0];
+            let op = eqArray[1];
+            let nextNumber = eqArray[2];
+            let currentSum = operate(current, op, nextNumber);
+            sum += currentSum;
+            eqArray.splice(0, 2);
+            return sum;
+        }, 0);
+    }
+    display.textContent = answer;
 }
 
 const display = document.querySelector('#display');
@@ -20,23 +42,21 @@ numbers.forEach((number) => {
         display.textContent += number.id;
     });
 });
+
 const opButtons = document.querySelectorAll('.operator');
 opButtons.forEach((button) => {
     button.addEventListener('click', () => {
         display.textContent += ' ' + button.id + ' ';
     });
 });
-const equals = document.querySelector('#equals');
-equals.addEventListener('click', () => {
-    let equation = display.textContent;
-    console.log(equation);
-    let eqArray = equation.split(' ');
-    console.log(eqArray);
-    let answer = operate(eqArray[0], eqArray[1], eqArray[2]);
-    console.log(answer);
-    display.textContent = answer;
-});
+
+const equalsBtn = document.querySelector('#equals');
+equalsBtn.addEventListener('click', equals);
+
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', () => {
     display.textContent = '';
-})
+});
+
+display.textContent = '3 + 3 + 3';
+console.log(equals())
