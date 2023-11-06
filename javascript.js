54fb8ef16902051b0d5d8157bfbf4229d24b3fec
Dexter Crago <jodieanddexter@gmail.com>
2023-11-06 16:22:07 -0700

    Improve code structure and addOperator functiona
    I felt like it made more sense to put the functions towards the bottom.
    Now, if a user tries to input two operators in a row, instead of simply
    denying the addition it will instead change the last operator to the
    new one the user clicked.

--- javascript.js
+++ javascript.js
@@ -9,6 +9,69 @@ function operate(a, op, b) {
     return operators[op](+a, +b);
 };
 
+const display = document.querySelector('#display');
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        let numberToAdd = number.id;
+        addNumber(numberToAdd);
+    });
+});
+
+const operatorBtns = document.querySelectorAll('.operator');
+operatorBtns.forEach((operatorBtn) => {
+    operatorBtn.addEventListener('click', () => {
+        let operatorToAdd = operatorBtn.id;
+        addOperator(operatorToAdd);
+    });
+});
+
+const equalsBtn = document.querySelector('#equals');
+equalsBtn.addEventListener('click', equals);
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    display.textContent = '';
+});
+
+const decimal = document.querySelector('#decimal');
+decimal.addEventListener('click', addDecimal);
+
+const backspace = document.querySelector('#backspace');
+backspace.addEventListener('click', backSpace);
+
+function addNumber(number) {
+    if (display.textContent === 'Don\'t divide by zero!') {
+        display.textContent = '';
+    }
+    display.textContent += number;
+}
+
+function addOperator(operator) {
+    console.log(display.textContent.charAt(display.textContent.length-1));
+    if (display.textContent.length > 4 && display.textContent.charAt(display.textContent.length-1) === ' ') {
+        let str = display.textContent;
+        display.textContent = str.slice(0, -3);
+        display.textContent += ' ' + operator + ' ';
+    } else if (display.textContent !== '' && display.textContent.charAt(display.textContent.length-1) !== '.') { //don't start equation with operator && don't allow two operators in a row && don't allow a number to end with a decimal
+        display.textContent += ' ' + operator + ' ';
+    }
+}
+
+function addDecimal() {
+    if (display.textContent.includes('.') === false) {
+        display.textContent += '.';
+    }
+}
+
+function backSpace() {
+    let str = display.textContent;
+    if (str.slice(-1) === ' ') {
+        display.textContent = str.slice(0, -3);
+    } else display.textContent = str.slice(0, -1);
+}
+
 function equals() {
     let equation = display.textContent;
     let equationArr = equation.split(' ');
@@ -23,63 +86,6 @@ function equals() {
     } else display.textContent = Math.round(answer * 100) / 100;
 }
 
-const display = document.querySelector('#display');
-const numbers = document.querySelectorAll('.number');
-numbers.forEach((number) => {
-    number.addEventListener('click', () => {
-        let numberToAdd = number.id;
-        addNumber(numberToAdd);
-    });
-});
-
-function addNumber(number) {
-    if (display.textContent === 'Don\'t divide by zero!') {
-        display.textContent = '';
-    }
-    display.textContent += number;
-}
-
-const opButtons = document.querySelectorAll('.operator');
-opButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        let operatorToAdd = button.id;
-        addOperator(operatorToAdd);
-    });
-});
-
-function addOperator(operator) {
-    if (display.textContent !== '' && display.textContent.charAt(display.textContent.length-1) !== ' ' && display.textContent.charAt(display.textContent.length-1) !== '.') { //don't start equation with operator && don't allow two operators in a row && don't allow a number to end with a decimal
-        display.textContent += ' ' + operator + ' ';
-    }
-}
-
-const equalsBtn = document.querySelector('#equals');
-equalsBtn.addEventListener('click', equals);
-
-const clear = document.querySelector('#clear');
-clear.addEventListener('click', () => {
-    display.textContent = '';
-});
-
-const decimal = document.querySelector('#decimal');
-decimal.addEventListener('click', addDecimal);
-
-function addDecimal() {
-    if (display.textContent.includes('.') === false) {
-        display.textContent += '.';
-    }
-}
-
-const backspace = document.querySelector('#backspace');
-backspace.addEventListener('click', backSpace);
-
-function backSpace() {
-    let str = display.textContent;
-    if (str.slice(-1) === ' ') {
-        display.textContent = str.slice(0, -3);
-    } else display.textContent = str.slice(0, -1);
-}
-
 const body = document.querySelector('body');
 body.addEventListener('keydown', () => {
     let key = event.key;
@@ -90,7 +96,7 @@ body.addEventListener('keydown', () => {
     if (key === '1' || key === '2' || key === '3' || key === '4' || key === '5' || key === '6' || key === '7' || key === '8' || key === '9' || key === '0') {
         addNumber(key);
     }
-    if (key === '+' || key === '-' || key === '*' || key === '/') { //come back for '/'
+    if (key === '+' || key === '-' || key === '*' || key === '/') {
         addOperator(key);
     }
     if (key === '.') {
