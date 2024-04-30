45e76f3de6eb7d3b7844009ea308b0abe08804e4
Dexter Crago <jodieanddexter@gmail.com>
2024-04-29 20:21:35 -0600

    Remove old files

--- javascript.js
+++ javascript.js
@@ -1,124 +1,120 @@
-let operators = {
+let previousNumber;
+let nextNumber;
+let currentOperator;
+
+const operations = {
   '+': (a, b) => a + b,
   '-': (a, b) => a - b,
   '*': (a, b) => a * b,
   '/': (a, b) => a / b,
 };
 
-function operate(a, op, b) {
-  return operators[op](+a, +b);
-}
-
 const display = document.querySelector('#display');
 display.textContent = '0';
 
-const numbers = document.querySelectorAll('.number');
-numbers.forEach((number) => {
+document.querySelectorAll('.number').forEach((number) => {
   number.addEventListener('click', () => {
-    let numberToAdd = number.id;
-    addNumber(numberToAdd);
+    updateDisplay(number.textContent);
   });
 });
 
-const operatorBtns = document.querySelectorAll('.operator');
-operatorBtns.forEach((operatorBtn) => {
-  operatorBtn.addEventListener('click', () => {
-    let operatorToAdd = operatorBtn.id;
-    addOperator(operatorToAdd);
+document.querySelectorAll('.operator').forEach((operator) => {
+  operator.addEventListener('click', () => {
+    setCurrentOperator(operator.textContent);
   });
 });
 
-const equalsBtn = document.querySelector('#equals');
-equalsBtn.addEventListener('click', equals);
+document.querySelector('#equals').addEventListener('click', displayAnswer);
 
-const clear = document.querySelector('#clear');
-clear.addEventListener('click', () => {
-  display.textContent = '0';
+document.querySelector('#clear').addEventListener('click', clearDisplay);
+
+document.querySelector('#decimal').addEventListener('click', addDecimal);
+
+document.querySelector('#backspace').addEventListener('click', removeNumber);
+
+document.querySelector('body').addEventListener('keydown', (event) => {
+  keyboardSupport(event.key);
 });
 
-const decimal = document.querySelector('#decimal');
-decimal.addEventListener('click', addDecimal);
-
-const backspace = document.querySelector('#backspace');
-backspace.addEventListener('click', backSpace);
-
-function addNumber(number) {
-  if (
-    display.textContent === '0' ||
-    display.textContent === "Don't divide by zero!"
-  ) {
-    display.textContent = '';
-  }
-  display.textContent += number;
+function operate(a = 0, b = 0, op = '+') {
+  return operations[op](+a, +b);
 }
 
-function addOperator(operator) {
-  if (
-    display.textContent.length > 0 &&
-    display.textContent.charAt(display.textContent.length - 1) === ' '
-  ) {
-    //if user tries to input two operators in a row, change the current operator to the new one
-    let str = display.textContent;
-    display.textContent = str.slice(0, -3);
+function setPreviousNumber(number) {
+  previousNumber = number;
+}
+
+function setNextNumber(number) {
+  nextNumber = number;
+}
+
+function setCurrentOperator(number) {
+  currentOperator = number;
+}
+
+function newEquation() {
+  setPreviousNumber(display.textContent);
+  setNextNumber(undefined);
+  setCurrentOperator(undefined);
+}
+
+function updateDisplay(number) {
+  if (currentOperator === undefined) {
+    display.textContent === '0'
+      ? (display.textContent = number)
+      : (display.textContent += number);
+    setPreviousNumber(display.textContent);
   }
-  display.textContent += ' ' + operator + ' ';
+  if (currentOperator !== undefined) {
+    display.textContent === previousNumber
+      ? (display.textContent = number)
+      : (display.textContent += number);
+    setNextNumber(display.textContent);
+  }
+}
+
+function displayAnswer() {
+  display.textContent = operate(previousNumber, nextNumber, currentOperator);
+  newEquation();
+}
+
+function clearDisplay() {
+  display.textContent = '0';
+  newEquation();
 }
 
 function addDecimal() {
-  if (display.textContent.includes('.') === false) {
+  if (!display.textContent.includes('.')) {
     display.textContent += '.';
   }
 }
 
-function backSpace() {
-  let str = display.textContent;
-  if (str.slice(-1) === ' ') {
-    display.textContent = str.slice(0, -3);
-  } else if (str.length === 1) {
-    display.textContent = '0';
-  } else display.textContent = str.slice(0, -1);
+function removeNumber() {
+  display.textContent.length === 1
+    ? (display.textContent = '0')
+    : (display.textContent = display.textContent.slice(0, -1));
+
+  currentOperator === undefined
+    ? setPreviousNumber(display.textContent)
+    : setNextNumber(display.textContent);
 }
 
-function equals() {
-  let equation = display.textContent;
-  let equationArr = equation.split(' ');
-  let answer = operate(equationArr[0], equationArr[1], equationArr[2]);
-  equationArr.splice(0, 3);
-  while (equationArr.length > 0) {
-    answer = operate(answer, equationArr[0], equationArr[1]);
-    equationArr.splice(0, 2);
+function keyboardSupport(eventKey) {
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  const operators = ['+', '-', '*', '/'];
+  if (numbers.includes(eventKey)) {
+    updateDisplay(+eventKey);
   }
-  if (answer == 'Infinity' || answer == '-Infinity' || isNaN(answer)) {
-    display.textContent = "Don't divide by zero!";
-  } else display.textContent = Math.round(answer * 100) / 100;
-}
-
-//keyboard support
-const body = document.querySelector('body');
-body.addEventListener('keydown', (event) => {
-  let key = event.key;
-  if (key === 'Tab') {
-    event.preventDefault();
-  } else if (
-    key === '1' ||
-    key === '2' ||
-    key === '3' ||
-    key === '4' ||
-    key === '5' ||
-    key === '6' ||
-    key === '7' ||
-    key === '8' ||
-    key === '9' ||
-    key === '0'
-  ) {
-    addNumber(key);
-  } else if (key === '+' || key === '-' || key === '*' || key === '/') {
-    addOperator(key);
-  } else if (key === '.') {
+  if (operators.includes(eventKey)) {
+    setCurrentOperator(eventKey);
+  }
+  if (eventKey === '.') {
     addDecimal();
-  } else if (key === 'Enter' || key === '=') {
-    equals();
-  } else if (key === 'Backspace') {
-    backSpace();
   }
-});
+  if (eventKey === 'Enter' || eventKey === '=') {
+    displayAnswer();
+  }
+  if (eventKey === 'Backspace') {
+    removeNumber();
+  }
+}
